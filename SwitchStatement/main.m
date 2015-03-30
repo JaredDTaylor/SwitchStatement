18d656265820581a40a4e5c55da7ad3a151a1b2d
Jared <jaredtaylor@cbrc.net>
2015-03-30 11:36:01 -0400

    switch statement tests

--- SwitchStatement/main.m
+++ SwitchStatement/main.m
@@ -10,8 +10,28 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        int userInput = 10;
+        switch (userInput) {
+            case 1:
+            case 2:
+            case 3:
+                NSLog(@"Low");
+                break;
+            case 4:
+            case 5:
+            case 6:
+                NSLog(@"Middle");
+                break;
+            case 7:
+            case 8:
+            case 9:
+                NSLog(@"High");
+                break;
+            default:
+                NSLog(@"Other");
+                break;
+        }
+        
     }
     return 0;
 }
